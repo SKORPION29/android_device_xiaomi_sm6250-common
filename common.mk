f64170a2daa96955e9992356d65115fe88371f02
SKORPION29 <sayansarkar29.slg@gmail.com>
2022-06-03 21:51:46 +0530

    sm6250-common: aex bringup

--- common.mk
+++ common.mk
@@ -323,7 +323,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-blaze
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
